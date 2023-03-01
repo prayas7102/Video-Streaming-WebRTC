1ea1a7fb981ebfbaff9a79bec3867de5badcf589
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-03-02 00:18:19 +0530

    tailwind and socket setup done

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,4 +4,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
+const cors_1 = __importDefault(require("cors"));
+// import { roomHandler } from "./room";
 const app = (0, express_1.default)();
+app.use(cors_1.default);
+const port = 8080;
+const server = http_1.default.createServer(app);
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    // roomHandler(socket);
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+});
+server.listen(port, () => {
+    console.log(`listening on *:${port}`);
+});
